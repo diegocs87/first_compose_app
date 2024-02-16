9d44c7a13d015f868607286196d81adee36e4063
Diego Cardenas <dcardenas@veritran.com>
2024-02-15 22:06:29 -0500

    Se organiza boton y Text en la pantalla. Se cambian sus atributos.

--- app/src/main/java/com/example/first_compose_app/MainActivity.kt
+++ app/src/main/java/com/example/first_compose_app/MainActivity.kt
@@ -6,16 +6,24 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.first_compose_app.ui.theme.First_compose_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,12 +46,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier.background(Color.Magenta),
-    )
-    Button(onClick = { onClick() }) {
-
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize().background(Color.LightGray))
+    {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.background(Color.Magenta).padding(15.dp)
+        )
+        ElevatedButton(onClick = { onClick() },
+            colors = ButtonDefaults.buttonColors(
+            contentColor = Color.Black)) {
+            Text("Get Proverbs",
+                color = Color.Blue,
+                fontFamily = FontFamily.SansSerif)
+        }
     }
 }
 
