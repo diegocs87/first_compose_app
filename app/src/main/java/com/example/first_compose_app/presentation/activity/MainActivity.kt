1e33ac9f22ab07fe667bbefac9ad36142db0f2f1
Diego Cardenas <dcardenas@veritran.com>
2024-02-20 23:25:13 -0500

    Se separa el proyecto en pauqetes y se distribuyen los composables en archivos.

--- app/src/main/java/com/example/first_compose_app/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/first_compose_app/presentation/activity/MainActivity.kt
@@ -1,8 +1,6 @@
-package com.example.first_compose_app
+package com.example.first_compose_app.presentation.activity
 
-import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -10,7 +8,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -20,11 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.first_compose_app.ui.theme.First_compose_appTheme
+import com.example.first_compose_app.presentation.composable_ui.composables.ProverbsButton
+import com.example.first_compose_app.presentation.composable_ui.theme.First_compose_appTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,24 +45,18 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column(verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize().background(Color.LightGray))
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray))
     {
         Text(
             text = "Hello $name!",
-            modifier = modifier.background(Color.Magenta).padding(15.dp)
+            modifier = modifier
+                .background(Color.Magenta)
+                .padding(15.dp)
         )
-        ElevatedButton(onClick = { onClick() },
-            colors = ButtonDefaults.buttonColors(
-            contentColor = Color.Black)) {
-            Text("Get Proverbs",
-                color = Color.Blue,
-                fontFamily = FontFamily.SansSerif)
-        }
+        ProverbsButton()
     }
-}
-
-private fun onClick(){
-
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/example/first_compose_app/presentation/composable_ui/screens/GetProverbsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.first_compose_app.presentation.composable_ui.screens
+
+import androidx.compose.runtime.Composable
+import com.example.first_compose_app.presentation.composable_ui.composables.ProverbsButton
+
+@Composable
+fun GetProverbsScreen(){
+    ProverbsButton ()
+}
