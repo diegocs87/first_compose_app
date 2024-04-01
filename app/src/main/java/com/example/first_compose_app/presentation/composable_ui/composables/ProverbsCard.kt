4c1ea124328d913923e88e9be99563333d83ec42
Diego Cardenas <dcardenas@veritran.com>
2024-03-31 21:32:27 -0500

    Primer diseño de tarjeta.

--- app/src/main/java/com/example/first_compose_app/presentation/composable_ui/composables/ProverbsCard.kt
+++ app/src/main/java/com/example/first_compose_app/presentation/composable_ui/composables/ProverbsCard.kt
@@ -1,22 +1,49 @@
 package com.example.first_compose_app.presentation.composable_ui.composables
 
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.first_compose_app.R
 
 @Composable
 fun ProverbsCard() {
+    val painterResource = painterResource(id = R.drawable.chelsea2)
     ElevatedCard() {
-        Text(
-            "hello card",
-            color = Color.Blue,
-            fontFamily = FontFamily.SansSerif,
-            modifier = Modifier.size(width = 240.dp, height = 100.dp)
-        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(painter = painterResource, contentDescription = "Chelsea F.C logo")
+            Column {
+                Text(
+                    "Chelsea F.C",
+                    color = Color.Blue,
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp,
+                    modifier = Modifier.padding(start = 15.dp, end = 15.dp)
+                )
+
+                Text(
+                    "Funded in: ",
+                    color = Color.Black,
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(start = 15.dp, end = 15.dp)
+                )
+            }
+        }
     }
 }
